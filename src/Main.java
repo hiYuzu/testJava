0e3b9d8241c58384055e393495436fec4556c63e
hiYuzu <pomelo.yuzu1997@gmail.com>
2022-09-22 14:28:32 +0800

    updater0922

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,59 @@
+import cn.hutool.core.thread.ThreadUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
 /**
  * @author hiYuzu
  * @version V1.0
  * @date 2022/3/10 16:07
  */
 public class Main {
-    public static void main(String[] args) throws Exception {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        testArrayList();
+//        testVector();
+    }
 
+    private static void testArrayList() {
+        System.out.println("start");
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            list.add(i);
+        }
+        ThreadUtil.execute(() -> {
+            list.removeIf(one -> one >= 50000);
+            System.out.println("array1线程执行完毕。数据量：" + list.size());
+        });
+        ThreadUtil.execute(() -> {
+            list.removeIf(one -> one >= 50000);
+            System.out.println("array2线程执行完毕。数据量：" + list.size());
+        });
+        ThreadUtil.execute(() -> {
+            list.removeIf(one -> one >= 50000);
+            System.out.println("array3线程执行完毕。数据量：" + list.size());
+        });
+    }
+
+    private static void testVector() {
+        System.out.println("start");
+        List<Integer> list = new Vector<>();
+        for (int i = 0; i < 100000; i++) {
+            list.add(i);
+        }
+        ThreadUtil.execute(() -> {
+            list.removeIf(one -> one >= 50000);
+            System.out.println("vector1线程执行完毕。数据量：" + list.size());
+        });
+        ThreadUtil.execute(() -> {
+            list.removeIf(one -> one >= 50000);
+            System.out.println("vector2线程执行完毕。数据量：" + list.size());
+        });
+        ThreadUtil.execute(() -> {
+            list.removeIf(one -> one >= 50000);
+            System.out.println("vector3线程执行完毕。数据量：" + list.size());
+        });
     }
 }
 
---
+++ src/http/Test.java
@@ -0,0 +1,55 @@
+package http;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <p>接口请求测试</p>
+ * <p>请将IP、端口、证件号码、授权码等替换为真实数据</p>
+ * <p>JDK版本：1.8</p>
+ *
+ * @author hiYuzu
+ * @version V1.0
+ * @date 2022/9/22 14:18
+ */
+public class Test {
+    public static void main(String[] args) throws Exception {
+        // IP地址
+        String ip = "188.1.128.218";
+        // 端口
+        String port = "8084";
+        // 证件号码
+        String identity = "370786199701155455";
+        // 授权码
+        String auth = "jlf5ydoq-u7dh-olrp-n2mk-a8lrc8q3nfkw";
+
+        HttpURLConnection con;
+        BufferedReader buffer;
+        StringBuilder resultBuilder;
+        try {
+            URL url = new URL("http://"+ ip + ":" + port + "/history/openApi/appeal/getPersonInfo?identity=" + identity);
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            con.setRequestProperty("IIG-AUTH", auth);
+            con.setDoOutput(true);
+            con.setUseCaches(false);
+            if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                InputStream inputStream = con.getInputStream();
+                resultBuilder = new StringBuilder();
+                String line;
+                buffer = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                while ((line = buffer.readLine()) != null) {
+                    resultBuilder.append(line);
+                }
+                System.out.println(resultBuilder);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
