6ff35565e6b4c2a8eb6424aa5bfa6b36c9156043
hiYuzu <pomelo.yuzu1997@gmail.com>
2023-05-19 18:24:10 +0800

    update0519

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,9 @@
-import cn.hutool.core.util.RandomUtil;
-import cn.hutool.json.JSONUtil;
-import com.alibaba.fastjson.JSONObject;
-
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author hiYuzu
  * @version V1.0
  * @date 2022/3/10 16:07
  */
 public class Main {
-    public static void main(String[] args) {
-        String text = "aaaaaa";
-        byte[] utf8Bytes = text.getBytes(StandardCharsets.UTF_8);
-        String utf8Text = new String(utf8Bytes, StandardCharsets.UTF_8);
-        String isoText = new String(utf8Bytes, StandardCharsets.ISO_8859_1);
-        System.out.println("UTF-8:::" + utf8Text);
-        System.out.println("ISO_8859_1:::" + isoText);
+    public static void main(String[] args) throws Exception {
     }
 }
--- src/bjcdc/TestYqjc.java
+++ src/bjcdc/TestYqjc.java
@@ -35,8 +35,8 @@ public class TestYqjc {
     public static void main(String[] args) throws Exception {
         // 传参Map
         Map<String, Object> paramMap = new HashMap<>(9);
-        paramMap.put("startDate", "2023-05-16");
-        paramMap.put("endDate", "2023-05-16");
+        paramMap.put("startDate", "2023-05-18");
+        paramMap.put("endDate", "2023-05-18");
 
         final int timeout = 20000;
         String responseBody = HttpUtil.createPost(SYS_PROTOCOL + "://" + SYS_IP + ":" + SYS_PORT + SYS_METHOD)
