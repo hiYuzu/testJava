dce52e8936cf5e3409209cf8abfd1ea73aa9201a
hiYuzu <pomelo.yuzu1997@gmail.com>
2026-02-04 11:20:47 +0800

    update0204

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-import cipher.annotation.CipherField;
-import cipher.annotation.HmacField;
-import cipher.util.CipherUtil;
-import cn.hutool.json.JSONUtil;
+import cn.hutool.core.date.DateUtil;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -16,54 +11,8 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger logger = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) throws Exception {
-        CipherUtil<User> cipherUtil = new CipherUtil<>();
-        User user1 = new User("1", "张三");
-        User user2 = new User("2", "李四");
-        List<User> userList = new ArrayList<>();
-        userList.add(user1);
-        userList.add(user2);
-        cipherUtil.hmac(userList);
-        logger.log(Level.INFO, JSONUtil.toJsonPrettyStr(userList));
-        logger.log(Level.INFO, JSONUtil.toJsonPrettyStr(cipherUtil.verifyHmac(userList)));
-        userList.get(0).setName("王五");
-        logger.log(Level.INFO, JSONUtil.toJsonPrettyStr(cipherUtil.verifyHmac(userList)));
-
-    }
-
-    public static class User {
-        private String id;
-        @CipherField
-        private String name;
-        @HmacField
-        private String hmac;
-
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getHmac() {
-            return hmac;
-        }
-
-        public void setHmac(String hmac) {
-            this.hmac = hmac;
-        }
-
-        public User(String id, String name) {
-            this.id = id;
-            this.name = name;
-        }
+        Date date1 = DateUtil.parseDateTime("2025-03-27 15:32:56");
+        Date date2 = DateUtil.parseDateTime("2026-02-04 13:32:56");
+        logger.warning(String.valueOf(DateUtil.betweenDay(date1, date2, true)));
     }
 }
--- src/UuidGenerator.java
+++ src/UuidGenerator.java
@@ -1,4 +1,6 @@
+import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.lang.UUID;
+import cn.hutool.core.util.IdUtil;
 
 /**
  * @author hiYuzu
@@ -7,8 +9,15 @@ import cn.hutool.core.lang.UUID;
  */
 public class UuidGenerator {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 30; i++) {
             System.out.println(UUID.fastUUID().toString(false));
+        }
+        for (int i = 0; i < 30; i++) {
+            System.out.println(UUID.fastUUID().toString(true));
+        }
+        Snowflake snowflake = IdUtil.getSnowflake(1, 1);
+        for (int i = 0; i < 30; i++) {
+            System.out.println(snowflake.nextId());
         }
     }
 }
